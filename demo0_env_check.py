4806ff2aaa3ce002d083263380d278a8046b530f
tinalin <wang23460@gmail.com>
2022-05-23 14:56:20 +0800

    environment confirm renew

--- demo0_env_check.py
+++ demo0_env_check.py
@@ -3,8 +3,10 @@ import os
 import tensorflow as tf
 import sklearn
 import numpy
+import pandas as pd
 print(sys.executable)
 print(os.getcwd())
 print(f"tensorflow version={tf.__version__}");
 print(f"sklearn version={sklearn.__version__}");
 print(f"numpy version={numpy.__version__}");
+print(f"pandas version={pd.__version__}");
