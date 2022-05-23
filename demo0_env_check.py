b53dcdabfa69210837c9afe410d1ddb7cb2f4b6f
tinalin <wang23460@gmail.com>
2022-05-23 14:07:57 +0800

    environment confirm

---
+++ demo0_env_check.py
@@ -0,0 +1,10 @@
+import sys
+import os
+import tensorflow as tf
+import sklearn
+import numpy
+print(sys.executable)
+print(os.getcwd())
+print(f"tensorflow version={tf.__version__}");
+print(f"sklearn version={sklearn.__version__}");
+print(f"numpy version={numpy.__version__}");
